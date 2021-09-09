548ef7d06d3176104cc827f2fbac6b84578535cc
Mateus T <mateustavarestv@hotmail.com>
2021-09-09 18:26:57 -0300

    primeira vez usando o material-ui

--- criando-rotas/src/Home.js
+++ criando-rotas/src/Home.js
@@ -1,13 +1,13 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import { Button } from '@material-ui/core'
 
 const Home = ()=>{
   return (
-    <ul>
-      <li>
-        <Link to="/users">Usuarios</Link>
-      </li>
-    </ul>
+    <>
+      <Button variant="contained" color="primary"><Link to="/users">Usuarios</Link></Button>
+      <Button variant="contained" color="primary">Hello World</Button>;
+    </>
   )
 }
 
