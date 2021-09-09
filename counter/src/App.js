81c285cc7689dcf5dbfd54e3562c68ccac785866
Mateus T <mateustavarestv@hotmail.com>
2021-09-09 15:22:26 -0300

    usando ainda mais o useState, so que em navegacao

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import Counter from './Counter'
+import React, {useState} from 'react'
 
+import Counter from './Counter'
+import Albums from './Albums'
+
+const pages = {
+  Albums:{
+    text:"Albuns",
+    component: Albums,
+  },
+  Counter:{
+    text:"Contador",
+    component: Counter,
+  }
+}
 
 function App() {
+  const [page, setpage] = useState('Albums')
+  
+  const handleOnChange = (pageChosen) =>{
+    setpage(pageChosen)
+  }
+  
+  const Page = pages[page].component
+
+  const pagesNames = Object.keys(pages)
+  
   return (
     <>
-    <Counter />
+    {
+      pagesNames.map((page) => <button onClick = {
+        ()=>{handleOnChange(page)}
+      }>{pages[page].text}</button> )
+    }
+    {Page && <Page />}
     </>
   )
 }
