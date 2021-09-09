07768e8c85d6ad065e5f5dae1a5f359e2e98a60f
Mateus T <mateustavarestv@hotmail.com>
2021-09-09 14:23:04 -0300

    apredendo sobre useState

---
+++ counter/src/Counter.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import './Counter.css'
+
+const Counter =  () => {
+  const [contador,setContador] = useState(0)
+  
+  const handleClick = (count) => {
+    const somar = count === '+'? setContador(contador + 1): setContador(contador - 1)
+  }
+  
+  return(
+    <div className ="counter">
+      <span>{contador}</span>
+      <button onClick={()=>handleClick('-')}>-</button>
+      <button onClick={()=>handleClick('+')}>+</button>
+    </div>
+  )
+
+}
+
+export default Counter
