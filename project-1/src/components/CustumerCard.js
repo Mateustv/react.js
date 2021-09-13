35441f3b36b003c309b268a13f679c88782db889
Mateus Tavares <mateustavarestv@hotmail.com>
2021-09-13 16:41:44 -0300

    removendo os usarios por meio da API

--- project-1/src/components/CustumerCard.js
+++ project-1/src/components/CustumerCard.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React, {useState} from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import IconButton from '@material-ui/core/IconButton'
+
+import Typography from '@material-ui/core/Typography'
+import EditIcon from '@material-ui/icons/Edit'
+import ClearIcon from '@material-ui/icons/Clear'
+
+import ModalAlert from './ModalAlert'
 
 const useStyles = makeStyles({
   root: {
@@ -15,36 +21,51 @@ const useStyles = makeStyles({
   media: {
     height: 140,
   },
-});
+})
 
-export default function MediaCard({name, lastName, email, avatar}) {
-  const classes = useStyles();
+ const MediaCard = ({id, name, lastName, email, avatar, onRemoveCustomers}) => {
+  
+  const classes = useStyles()
+  const [useModal, setUseModal] = useState(false)
+
+  const handleToggleModal = () => {
+    setUseModal(!useModal)
+  }
+  const handleModal = (id) => {
+    onRemoveCustomers(id)
+    handleToggleModal()
+  }
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={avatar}
-          title= {`${name} ${lastName}`}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {`${name} ${lastName}`}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {email}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small">
-          Share
-        </Button>
-        <Button size="small">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    <>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={avatar}
+            title= {`${name} ${lastName}`}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {`${name} ${lastName}`}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {email}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <IconButton aria-label="Editar">            
+            <EditIcon />
+          </IconButton> 
+          <IconButton aria-label="Remover" onClick={handleToggleModal}>
+            <ClearIcon />          
+          </IconButton>
+        </CardActions>
+      </Card>
+      <ModalAlert open = {useModal} onClose ={handleToggleModal} onConfirm = {() => {handleModal(id)}} title = {'Deseja remover o usuario ?'} message = {'Apos remover o usario não terar mais voltar'}/>
+    </>
+  )
 }
+
+export default MediaCard
--- project-1/src/pages/Costumers.js
+++ project-1/src/pages/Costumers.js
@@ -6,6 +6,7 @@ import Grid from '@material-ui/core/Grid';
 
 const Costumers  = () => {
   const [costumers, setCostumers] = useState([])
+  
   useEffect(
     ()=>{
       axios.get('https://reqres.in/api/users?').then(costumersApi => {
@@ -15,6 +16,14 @@ const Costumers  = () => {
     },[]
   )
 
+  const handleRemoveCustomers  = (id) => {
+    axios.delete(`https://reqres.in/api/users?/${id}`).then(() => {
+      
+      const withoutCustumer = costumers.filter(user => user.id !== id)
+      setCostumers(withoutCustumer)
+    })
+  }
+
   return (    
     <>
       <Grid container spacing={5}>
@@ -22,10 +31,12 @@ const Costumers  = () => {
           costumers.map((item)=>(
             <Grid item xs ={12} md = {4}>
               <CustumerCard 
+                id = {item.id}
                 name ={item.first_name}
                 lastName = {item.last_name}
                 email = {item.email}
                 avatar = {item.avatar}
+                onRemoveCustomers = {handleRemoveCustomers}
               />
             </Grid>
           ))
