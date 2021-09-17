4a86584e88b4ce0b576b928890c1cf694c5afe23
Mateus Tavares <mateustavarestv@hotmail.com>
2021-09-17 09:51:35 -0300

    postando usuarios em uma api fake

--- project-1/src/pages/CostumersAdd.js
+++ project-1/src/pages/CostumersAdd.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 import TextField from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
@@ -10,21 +11,104 @@ const useStyles = makeStyles((theme) => ({
     }
   }))
 
-const CostumersAdd = () => {
-    const classe = useStyles()
+  
+  
+  
+  const CostumersAdd = () => {
+      const classe = useStyles()
+      
+      const [form, setForm] = useState({
+          name : {
+              value : '',
+              error : false,
+              helperText: '',
+          },
+          job : {
+              value : '',
+              error : false,
+              helperText: '',
+          },
+      })
+      
+
+      const handleChange = (state) => {
+        const {name, value} = state.target
+
+        setForm({
+            ...form,
+            [name]: {
+                value,
+            },
+        })
+      }
+
+      const handleClick = () => {
+        let hasError = false
+        
+        let setFormChange = {
+            ...form,
+        }
+
+        
+        if(!form.name.value){
+            hasError = true
+
+            setFormChange.name = {
+                value: form.name.value,
+                error: true,
+                helperText:"Campo com error",
+            }
+        }
+
+        if(!form.job.value){
+            hasError = true
+
+            setFormChange.job = {
+                value: form.job.value,
+                error: true,
+                helperText:'Campo com error',
+            }
+        }
+        
+        if(hasError){
+            return setForm(setFormChange) 
+        }
+        axios.post('https://reqres.in/api/users',{
+            nome:form.nome.value,
+            job:form.job.value,
+        }).then((response) => {console.log(response)})
+      }
     
     return (
-        <form>
+        <div>
             <div className = {classe.margins}>
-                <TextField id="outlined-basic" label="Digite seu nome" variant="outlined" />
+                <TextField 
+                 error ={form.name.error}
+                 helperText={form.name.error ? form.name.helperText : ''}
+                 id="outlined-basic"
+                 label="Digite seu nome"
+                 variant="outlined" 
+                 name="name" value={form.name.value} 
+                 onChange={handleChange}
+                />
             </div>
+
             <div className = {classe.margins}>
-                <TextField id="outlined-basic" label="Digite seu sobrenome" variant="outlined" />
+                <TextField 
+                 error ={form.job.error}
+                 helperText={form.job.error ? form.job.helperText : ''}
+                 id="outlined-basic"
+                 label="Digite seu Cargo"
+                 variant="outlined" name= "job"
+                 value={form.job.value} 
+                 onChange={handleChange}
+                />
             </div>
+
             <div className = {classe.margins}>
-                <Button variant="outlined">Enviar</Button>
+                <Button variant="outlined" onClick={handleClick}>Enviar</Button>
             </div>
-        </form>
+        </div>
     )
 }
 
