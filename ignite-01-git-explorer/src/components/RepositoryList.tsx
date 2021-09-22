cfa2f1e70ee9e73a7d5902639a3356cfee72bce9
Mateus Tavares <mateustavarestv@hotmail.com>
2021-09-22 20:01:59 -0300

    passando a aplicacao para typescript

--- ignite-01-git-explorer/src/components/RepositoryList.tsx
+++ ignite-01-git-explorer/src/components/RepositoryList.tsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import { RepositoryItem } from "./RepositoryItem";
+import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss'
 
 const repository = {
@@ -8,8 +8,14 @@ const repository = {
     link:'https://github.com/unform/unform',
 }
 
+interface Repository {
+    name: string
+    description: string
+    html_url: string
+}
+
 export function RepositoryList () {
-    const [repositories,setRepositories] = useState([])
+    const [repositories,setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
